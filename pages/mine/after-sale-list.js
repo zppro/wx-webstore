3f45339eb5f4379ede5f7811e18908221c527e1d
ahcl <ahcl4545@qq.com>
2017-01-17 15:20:03 +0800

    after-sale-list.js

--- pages/mine/after-sale-list.js
+++ pages/mine/after-sale-list.js
@@ -5,11 +5,11 @@ var app = getApp()
 Page({
     data: {
         isAppendDisabled: false,
-        isWaitAddNewAfterSaleInfo: false,
         pageOptions: {},
-        afterSale: [
-            // {id:"1",type:"我要换货",biz_status_name:"未处理",memo:"有明显破损",code:"123456",orderId:"1"},
-            // {id:"1",type:"我要换货",biz_status_name:"未处理",memo:"有明显破损",code:"123456"}
+        afterSales:[],
+        afterSalesTest: [
+            {id:"1",type:"我要换货",biz_status_name:"未处理",memo:"有明显破损",code:"123456",orderId:"1", audit_result_name:"已经通过", audit_comment:"未处理"},
+            {id:"1",type:"我要换货",biz_status_name:"未处理",memo:"有明显破损",code:"123456"}
         ],
         windowHeight: 627 - 45
     },
@@ -21,32 +21,21 @@ Page({
         if (this.data.isAppendDisabled) return;
         this.fetchData(false)
     },
-    onShow: function () {
-        if (this.data.isWaitAddNewAfterSaleInfo) {
-            let that = this
-            this.fetchData(true, () => {
-                // wx.stopPullDownRefresh()
-                that.setData({
-                    isWaitAddNewAfterSaleInfo: false
-                })
-            })
-        }
-    },
     //事件处理函数
     fetchData: function (refresh, cb) {
         let that = this;
-        let skip = refresh ? 0 : this.data.afterSale.length;
-        app.libs.http.post(app.config[keys.CONFIG_SERVER].getBizUrl() + 'afterSale/', {biz_status_name:this.data.pageOptions.biz_status_name, tenantId: app.config[keys.CONFIG_SERVER].getTenantId(), open_id: app.getSession().openid, page: { size: settings.LIST_PAGE_SIZE, skip } }, (afterSale) => {
+        let skip = refresh ? 0 : this.data.afterSales.length;
+        app.libs.http.post(app.config[keys.CONFIG_SERVER].getBizUrl() + 'afterSales/', { tenantId: app.config[keys.CONFIG_SERVER].getTenantId(), open_id: app.getSession().openid, page: { size: settings.LIST_PAGE_SIZE, skip } }, (afterSales) => {
             that.setData({
-                isAppendDisabled: afterSale.length == 0,
-                afterSale: refresh ? afterSale : that.data.afterSale.concat(afterSale)
+                isAppendDisabled: afterSales.length == 0,
+                afterSales: refresh ? afterSales : that.data.afterSale.concat(afterSales)
             })
         })
         if (cb && typeof cb == 'function') cb()
     },
-    orderTap: function (e) {
+    afterSaleTap: function (e) {
         wx.navigateTo({
-            url: './order-details?orderId=' + e.currentTarget.dataset.orderId
+            url: '../store/order-details?orderId=' + e.currentTarget.dataset.orderId
         })
     },
     onLoad: function (options) {
